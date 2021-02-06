762cdc1624005718399227f5300a06bf67cacd14
sangsangga <wandaputrasangga@gmail.com>
2021-02-06 21:12:00 +0700

    fix link to stylesheet and js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Word Scrambler</title>
-  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  <link rel="stylesheet" href="/css/app.css">
   <meta name="csrf-token" content="{{ csrf_token() }}" />
 </head>
 
@@ -48,7 +48,7 @@
   @yield('content')
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
   <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-  <script src="{{ asset('js/word.js') }}" defer></script>
+  <script src="js/word.js" defer></script>
 
 </body>
 
